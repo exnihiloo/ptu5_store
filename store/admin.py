3434b211bfc286977d418d70aa7b37e23268ba4e
exnihiloo <deimante.jasaityte@gmail.com>
2022-11-28 16:59:02 +0200

    media root added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'artist', 'slug',
+                    'price', 'in_stock', 'created',
+                    'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(models.Category)
+admin.site.register(models.Product)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -7,8 +11,8 @@ class Category(models.Model):
     slug = models.SlugField(max_length = 255, unique = True)
 
     class Meta:
-        verbose_name = _('category')
-        verbose_name_plural = _('categories')
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
 
     def __str__(self):
         return self.name
@@ -21,4 +25,26 @@ class Product(models.Model):
         related_name = 'product', 
         on_delete = models.CASCADE
     )
-    
+    created_by = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE, 
+        related_name = 'creator'
+    )
+    name = models.CharField(_('name'), max_length = 255)
+    artist = models.CharField(_('artist'), max_length = 255, default='admin')
+    description = models.TextField(_('description'), blank = True)
+    image = models.ImageField(_('image'), upload_to = 'images/')
+    slug = models.SlugField(max_length = 255, unique = True)
+    price = models.DecimalField(_('price'), max_digits = 4, decimal_places = 2)
+    in_stock = models.BooleanField(_('in_stock'), default = True)
+    is_active = models.BooleanField(_('is_active'), default = True)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+        ordering = ('-created', )
+
+    def __str__(self):
+        return self.name
