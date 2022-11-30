052420ae18075e1fd06dd697004dd88a2b509e21
exnihiloo <deimante.jasaityte@gmail.com>
2022-11-30 10:41:22 +0200

    basket deletion and updates

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,5 @@
+from store.models import Product
+from decimal import Decimal
 
 class Basket():
     def __init__(self, request):
@@ -8,12 +10,47 @@ class Basket():
         self.basket = basket
 
     def add(self, product, qty):
-        product_id = product.id
+        product_id = str(product.id)
         if product_id not in self.basket:
             self.basket[product_id] = {'price' : str(product.price), 'qty' : int(qty)}
 
-        self.session.modified = True
+        self.save()
+
+
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     
     def __len__(self):
         return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.basket:
+            del self.basket[product_id]
+            self.save()
+
+    def update(self, product, qty):
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
+    
+    def save(self):
+        self.session.modified = True
+
+    
